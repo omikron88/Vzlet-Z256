b98e019bfca52b844eea48a08e7d58a50dfb4764
user <user@mini>
2022-05-23 22:18:58 +0200

    crtc update

--- src/machine/Crtc.java
+++ src/machine/Crtc.java
@@ -6,6 +6,7 @@
 package machine;
 
 import java.awt.image.BufferedImage;
+import java.awt.Graphics;
 
 /**
  *
@@ -23,16 +24,18 @@ public class Crtc {
     private BufferedImage i;
     private int index;
     private byte[] regs;
+    private int pocit;
     
     Crtc(Vzlet machine, Memory mem) {
         ma = machine;
         m = mem;
         i = ma.getImage();
         regs = new byte[16];
+        pocit=0;
     }
     
     public void reset() {
-
+      pocit=0;
     }
 
     public void setIndex(int ind) {
@@ -48,21 +51,33 @@ public class Crtc {
     }
     
     public void paint() {
-        int c,d,ad,bd,adr;
+        int c,d,ad,bd,adr,adl,adp,hh,llen,chrl,blsp,kpos;
+        boolean blik,zobk;
         int ink;
-        int q = 0x8000;
-                     // 0      1      2      3      4      5      6      7      8      9      10     11     12     13     14    15
-        int lines[] = {0x8000,0x8050,0x80a0,0x80f0,0x9040,0x9090,0x90e0,0xa030,0xa080,0xa0d0,0xb020,0xb070,0xb0c0,0xc010,0xc060,0xc0b0,
-                       0xd000,0xd050,0xd0a0,0xd0f0,0xe040,0xe090,0xe0e0,0xf030,0xf080,0xf0d0};  //25 hodnot
-        for (int y=0; y<25; y++) {
-            for (int z=0; z<12; z++) {
-             d = y*12+z + ofsy;
-             adr = lines[y]+z*256;
-             for (int x=0; x<80; x++) {
-                ad = m.readVram(adr) & 0xff;
-                bd = m.readVram(adr|0x10000) & 0xff;
-                adr++;if ((adr& 0xff)==0){ adr=adr+0xf00;}
-                c = x * 8 + ofsx;
+        String tx;
+        Graphics g = i.getGraphics();
+
+        adr=(this.regs[13]+256*this.regs[12])|0x8000;
+        llen=this.regs[1];  // 50h=80                // počet znaků na řádek
+        chrl=this.regs[9];  // 0Bh=11                // počet vertikálních linek (počítáno od 0 ?)
+        blsp = (this.regs[10]&0x40)==0 ? 16 : 32;    // rychlost blikání
+        blik = (this.regs[10]&0x20)==0x20;           // blikat kurzorem
+        zobk= !((!blik)&(blsp==32));                 // zobrazit kurzor
+        kpos= this.regs[15] + 256*this.regs[14];
+
+        tx="adr: "+adr+"  "+llen+","+chrl+" blik: "+blik+" blsp:"+blsp+" zobk: "+zobk+" kpos:"+kpos;  // pomocná informace
+        
+        adl=adr;    // odkud začít zobrazovat
+        for (int li=0; li<300; li++)
+         {
+          d=li+ofsy;    //Y souřadnice (0-299)+offset
+          adp=adl;      //uschovej začátek mikrořádku
+          for (int x=0; x<80; x++)
+            {
+                ad = m.readVram(adl);
+                bd = m.readVram(adl|0x10000);
+                adl++;if ((adl& 0xff)==0){ adl=adl+0xf00;}  // posun na další znak
+                c = x * 8 + ofsx;   // X souřadnice (0-639)+offset
                 ink = pal[((ad&0x80)>>7)|((bd&0x80)>>6)];
                 i.setRGB(c++, d, ink);
                 ink = pal[((ad&0x40)>>6)|((bd&0x40)>>5)];
@@ -78,9 +93,22 @@ public class Crtc {
                 ink = pal[((ad&0x02)>>1)|(bd&0x02)];
                 i.setRGB(c++, d, ink);
                 ink = pal[(ad&0x01)|((bd&0x01)<<1)];
-                i.setRGB(c++, d, ink);
-            }  //x
-         }  //z    
-        }     //y
+                i.setRGB(c++, d, ink);   
+            }
+          adl=adp;      //obnov začátek mikrořádku
+          adl=adl+0x100;  // posun na další mikrořádek
+          if (((adl&0xf00)>>8) > chrl){   // pokud je hotovo, posun na další řádek
+                 hh=(adr&0xf00)>>8;
+                 adr=adr+llen;
+                 if (((adr&0xf00)>>8)!=hh){adr=adr+0xf00;}
+                 if (adr>65535){adr=(adr&0x0fff)|0x8000;}
+                 adl=adr;
+                                      }
+         }
+        pocit++; if (pocit==2*blsp){pocit=0;}  // zvyš počítadlo frames pro blikání kurzoru - perioda je 2x blsp
+        if ((zobk)&((pocit < blsp)&(blik))){    // zobrazit kurzor, pokud je povoleno, a když bliká tak pouze v první půlce režimu 2x blsp
+            
+                                           }
+     g.drawString(tx, 5,290);           // zobraz pomocnou informaci
     }    // paint
 }
